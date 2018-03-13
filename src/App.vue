6289344001ddf919bcfa1de193ebdabd2863b791
bluesbonew <bluesbonewong@gmail.com>
2018-03-13 16:06:38 +0800

    add file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template xmlns:v-for="http://www.w3.org/1999/xhtml">
   <div id="app">
     <h1>{{title}}</h1>
-    <div class="newTask">
-      <input type="text" v-model="newTodo" @keyup.enter="createNewList">
-    </div>
-    <ul>
-      <li v-for="item in items" :class="{finished:item.isFinished}" v-on:click="toggleClass(item)">
-        {{item.label}}<input type="checkbox" id="checkbox" v-model="item.isFinished">
-        <p v-if="item.isFinished">{{item.createAt}}</p>
-        <p v-else>{{item.createAt}}</p>
-        <button v-on:click="removeItem(item)">X</button>
-      </li>
-    </ul>
+    <section id="logInAndSignUp">
+      <div>
+        <label><input type="radio" name="type" value="signUp" v-model="actionType">注册</label>
+        <label><input type="radio" name="type" value="login" v-model="actionType">登录</label>
+      </div>
+      <div class="signUp" v-if="actionType === 'signUp'">
+        <form v-on:submit.prevent=signUp>
+          <div class="formRow">用户名<input type="text" v-model="formData.userName"></div>
+          <div class="formRow">密码<input type="password" v-model="formData.password"></div>
+          <div class="formActions"><input type="submit" value="注册"></div>
+        </form>
+      </div>
+      <div class="login" v-if="actionType === 'login'">
+        <div class="formRow">用户名<input type="text"></div>
+        <div class="formRow">密码<input type="password"></div>
+        <div class="formActions"><input type="submit" value="登录"></div>
+      </div>
+    </section>
+    <section id="todo">
+      <div class="newTask">
+        <input type="text" v-model="newTodo" @keyup.enter="createNewList">
+      </div>
+      <ul>
+        <li v-for="item in items" :class="{finished:item.isFinished}" v-on:click="toggleClass(item)">
+          {{item.label}}<input type="checkbox" id="checkbox" v-model="item.isFinished">
+          <p v-if="item.isFinished">{{item.createAt}}</p>
+          <p v-else>{{item.createAt}}</p>
+          <button v-on:click="removeItem(item)">X</button>
+        </li>
+      </ul>
+    </section>
+
   </div>
 </template>
 
@@ -19,10 +40,15 @@
   export default {
     data() {
       return {
-        title: 'Hello,welcome to use the to do list',
+        title: 'Welcome,ToDoList',
         items: [],
         newTodo: '',
-        checked: false
+        checked: false,
+        actionType: 'signUp',
+        formData: {
+          userName: '',
+          password: ''
+        }
       }
     },
     created: function () {
